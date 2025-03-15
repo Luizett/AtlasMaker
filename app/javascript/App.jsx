721495d7e48332b51d70c9d5b1c44935e2d0f6b9
Lisa <92753486+Luizett@users.noreply.github.com>
2025-03-15 12:27:22 +0300

    set up theme

--- app/javascript/App.jsx
+++ app/javascript/App.jsx
@@ -4,10 +4,10 @@ function App() {
       alert("Hello from React!")
   }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-xl text-amber-700">Hello, world!
+    <div className="flex flex-col items-center justify-center min-h-screen bg-timberwolf">
+      <h1 className="text-xl text-amber-700 font-roboto">Hello, world!</h1>
 
-      <p className="text-gray-700">This is a simple React app using Tailwind CSS.</p>
+      <p className="text-gray-700 font-unbounded">This is a simple React app using Tailwind CSS.</p>
       <ul className="list-disc list-inside">
         <li>Item 1</li>
         <li>Item 2</li>
@@ -16,7 +16,7 @@ function App() {
       <button onClick={click} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Click Me
       </button>
-      <p className="text-gray-500">This is a paragraph with some gray text.</p>  </h1>
+      <p className="text-gray-500">This is a paragraph with some gray text.</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 module.exports = {
     content: [
-      './app/views/**/*.html.erb',
-      './app/helpers/**/*.rb',
-      './app/assets/stylesheets/**/*.css',
-      './app/javascript/**/*.js',
-      './app/javascript/**/*.jsx'
-    ]
-  }
+        './app/views/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/assets/stylesheets/**/*.css',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.jsx'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'russian-violet': '#231942',
+                'ultra-violet': '#5E548E',
+                'lavander': '#9F86C0',
+                'african-violet': '#9B7DA8',
+                'lilac': '#BE95C4',
+                'pink': '#E0B1CB',
+                'eminence': '#71388A',
+                'amethyst': '#9665AB',
+                'indigo': '#440381',
+
+                'english-violet': '#55445C',
+                'raisin-black': '#322A36',
+
+                'light-gray': '#EBEBEB',
+                'timberwolf': '#D3D0CB',
+            },
+            fontFamily: {
+                unbounded: ['Unbounded', 'sans-serif'],
+                roboto: ['Roboto', 'sans-serif']
+            },
+        }
+    }
+}
