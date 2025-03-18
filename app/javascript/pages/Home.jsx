f7957ed898153f9492d581e4a62ac8a675ebc677
Lisa <92753486+Luizett@users.noreply.github.com>
2025-03-18 22:41:49 +0300

    Add home page view design

--- app/javascript/pages/Home.jsx
+++ app/javascript/pages/Home.jsx
@@ -6,24 +6,39 @@ const Home = () => {
     return (
         <>
             <Header />
-            <div className="bg-russian-violet min-h-screen ">
-                <h1 className="text-white text-5xl">
-                    GENERATE YOUR
-                    <span className="text-pink text-8xl">
+            <div className="bg-russian-violet min-h-screen flex flex-col align-middle">
+                <div className="w-4/5 mx-auto">
+                    <h1 className="text-white text-5xl font-bold font-unbounded mt-60 w-auto">
+                        GENERATE YOUR<br/>
+                        <span className="text-pink text-8xl mt-4 inline-block">
                         TEXTURE ATLAS
                     </span>
-                </h1>
-                <p className="text-xl">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <Link to="/images" className="bg-lilac">START NOW</Link>
-                <p className="text-timberwolf text-base">
-                    or <Link to="/" >Log In</Link> to save your works
-                </p>
+                    </h1>
+                    <p className="text-xl font-unbounded text-timberwolf mt-8 mb-20">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat.
+                    </p>
+
+                    <Link to="/images"
+                          className="bg-lilac font-unbounded  text-white tracking-widest font-medium
+                                     px-7 py-4 place-self-center justify-self-center
+                                      rounded-2xl block
+                                      shadow-pink shadow-button">
+                            START NOW
+                    </Link>
+
+
+                    <p className="text-timberwolf text-base text-center mt-10 font-unbounded leading-2 font-light">
+                        {"or "}
+                        <Link to="/"
+                            className="font-normal underline">
+                            Log In
+                        </Link>
+                        {" to save your works"}
+                    </p>
+                </div>
+
             </div>
         </>
     );
--- app/javascript/pages/_Header.jsx
+++ app/javascript/pages/_Header.jsx
@@ -4,17 +4,18 @@ import {Link} from "react-router";
 const Header = () => {
     return (
       <div className="flex flex-row justify-between align-middle
-                      w-full h-20 py-6 px-12
+                      w-full h-auto py-6 px-12
                       bg-ultra-violet ">
           <Link to="/" className="text-4xl text-light-gray font-bold inline">
               AtlasMaker
           </Link>
-          <div className=" gap-7">
+          <div className="flex flex-row gap-7">
               <button>theme</button>
               <button>lang</button>
               <Link to="/" className="bg-timberwolf rounded-full">
-                  <img src="../../assets/images/icons/user.svg" alt="user icon"/>
+                  <img src="/icons/user.png" alt="user icon" className="w-11 h-11"/>
               </Link>
+
           </div>
       </div>
     );
