5d3ac3c3698f037fe1e162c84f0bda0078487dc6
Lisa <92753486+Luizett@users.noreply.github.com>
2025-04-11 15:59:12 +0300

    Add bookshelf atlas sprite adding

--- app/controllers/sprite_controller.rb
+++ app/controllers/sprite_controller.rb
@@ -137,6 +137,99 @@ private
 
   def create_bookshelf
 
+    shelves = @atlas.coords["shelves"]
+    coords = @atlas.coords["coords"]
+
+    atlas_img = MiniMagick::Image.open(url_for(@atlas.atlas_img))
+    atlas_width = atlas_img.width
+    atlas_height = atlas_img.height
+
+    sprite_img = MiniMagick::Image.open(url_for(@sprite.sprite_img))
+    sprite_width = sprite_img.width
+    sprite_height = sprite_img.height
+
+    shelf = shelves.find { |sh| sh["height"] >= sprite_height && sprite_height.to_f/sh["height"] > 0.7}
+
+    if shelf
+      # insert to shelf
+      coord = {
+        id: @sprite.id,
+        start_height: shelf["start_height"],
+        start_width: shelf["width"],
+        height: sprite_height,
+        width: sprite_width
+      }
+      coords.push(coord)
+
+      shelf["width"] += sprite_width
+      if shelf["width"] > atlas_width
+        atlas_width = shelf["width"]
+
+        atlas_img.combine_options do |c|
+          c.background "none"
+          c.extent "#{atlas_width}x#{atlas_height}"
+        end
+      end
+
+      atlas_img.colorspace "sRGB"
+      atlas_img.alpha "on"
+      atlas_img = atlas_img.composite(sprite_img) do |c|
+        c.compose "Over"
+        c.geometry "+#{coord[:start_width]}+#{coord[:start_height]}"
+        c.alpha "on"
+        c.colorspace "sRGB"
+      end
+
+    else
+      # create new shelf
+
+      shelf = {
+        start_height: atlas_height,
+        height: sprite_height,
+        width: sprite_width
+      }
+      shelves.push(shelf)
+
+      coord = {
+        id: @sprite.id,
+        start_height: shelf[:start_height],
+        start_width: 0,
+        height: sprite_height,
+        width: sprite_width
+      }
+      coords.push(coord)
+
+      atlas_height += shelf[:height]
+      if atlas_width < shelf[:width]
+        atlas_width = shelf[:width]
+      end
+
+      atlas_img.combine_options do |c|
+        c.background "none"
+        c.extent "#{atlas_width}x#{atlas_height}"
+      end
+
+      atlas_img.colorspace "sRGB"
+      atlas_img.alpha "on"
+
+      atlas_img = atlas_img.composite(sprite_img) do |c|
+        c.compose "Over"
+        c.geometry "+0+#{shelf[:start_height]}"
+        c.alpha "on"
+        c.colorspace "sRGB"
+      end
+
+    end
+
+    @atlas.atlas_img.attach(
+      io: File.open(atlas_img.path),
+      filename: @atlas.title + ".png",
+      content_type: "image/png"
+    )
+
+    @atlas.coords = { type: "bookshelf", coords: coords, shelves: shelves }
+    @atlas.save
+
   end
 
   def create_skyline
