d39b3ff51883e7f74b52410e2fea592966d4b275
Lisa <92753486+Luizett@users.noreply.github.com>
2025-04-16 19:12:15 +0300

    Update fetch handler

--- app/controllers/atlases_controller.rb
+++ app/controllers/atlases_controller.rb
@@ -469,8 +469,6 @@ class AtlasesController < ApplicationController
           next_skyline[:width] = next_skyline[:end_width] - next_skyline[:start_width]
         end
 
-
-
       end
 
     end
@@ -483,7 +481,6 @@ class AtlasesController < ApplicationController
 
     @atlas.coords = { type: "skyline", coords: coords, skylines: skylines }
     @atlas.save
-
 
     render json: { message: "Atlas successfully updated.", atlas_img: url_for(@atlas.atlas_img) }
   end
@@ -524,84 +521,13 @@ class AtlasesController < ApplicationController
       end
       line[:width] = line[:end_width] - line[:start_width]
 
-
-      # line = skylines[i]
-      # left_neighbour = skylines[i - 1]
-      # right_neighbour = skylines[i + 1]
-      # line[:start_width] = left_neighbour[:start_width] if left_neighbour && left_neighbour[:start_height] < line[:start_height]
-      # line[:end_width] = right_neighbour[:end_width] if right_neighbour &&  right_neighbour[:start_height] < line[:start_height]
-      # line[:width] = line[:end_width] - line[:start_width]
-
       if line[:width] >= sprite_width
         return line
       end
     end
 
-    # skylines.each do |skyline|
-    #   if skyline[:start_height] <= current_skyline[:start_height]
-    #     # текущую линию можно продолжить
-    #     current_skyline[:end_width] = skyline[:end_width]
-    #     current_skyline[:width] = current_skyline[:end_width] - current_skyline[:start_width]
-    #
-    #   else
-    #     # линия поднялась выше чем предыдущая, значит старая завершена
-    #     current_skyline = skyline.dup
-    #   end
-    #
-    #   # сравнение current с best
-    #   if current_skyline[:width] >= sprite_width
-    #     if !best_skyline ||
-    #       current_skyline[:start_height] < best_skyline[:start_height] ||
-    #       (current_skyline[:start_height] == best_skyline[:start_height] && current_skyline[:width] < best_skyline[:width])
-    #       best_skyline = current_skyline.dup
-    #     end
-    #   end
-    # end
-    #best_skyline
+
     nil
   end
 
-  # def create
-  #   file1 = params[:img1]
-  #   file2 = params[:img2]
-  #
-  #
-  #   image11 = MiniMagick::Image.open(file1.file.path)
-  #   image22 = MiniMagick::Image.open(file2.file.path)
-  #
-  #   width = [image11.width, image22.width].max
-  #   height = image11.height + image22.height
-  #
-  #   # Создаем новое изображение с размерами, достаточными для склейки
-  #   result_image = MiniMagick::Image.new("result.jpg") do |img|
-  #     img.combine_options do |c|
-  #       c.size "#{width}x#{height}"
-  #       c.gravity "north"
-  #       c.background "white"
-  #       c.extent "#{width}x#{height}"
-  #     end
-  #   end
-  #
-  #   # Склеиваем изображения вертикально
-  #   result_image = result_image.composite(image1) do |c|
-  #     c.compose "Over"
-  #     c.geometry "+0+0"
-  #   end
-  #
-  #   result_image = result_image.composite(image2) do |c|
-  #     c.compose "Over"
-  #     c.geometry "+0+#{image1.height}"
-  #   end
-  #
-  #   # Сохраняем результат во временный файл
-  #   output_path = Rails.root.join("tmp", "combined_image.jpg")
-  #   result_image.write(output_path)
-  #
-  #   # Отправляем результат клиенту
-  #   send_file output_path, type: 'image/jpeg', disposition: 'inline'
-  #
-  #   file1.close
-  #   file2.close
-  #   File.delete(output_path) if File.exist?(output_path)
-  # end
 end
--- app/javascript/services/useFetch.js
+++ app/javascript/services/useFetch.js
@@ -1,9 +1,15 @@
 import React from "react";
 import {useSelector} from "react-redux";
-
+import store from "../slices/store";
+const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
 const useFetch = () => {
-    const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
-    const {token} = useSelector(state => state.session);
+
+    const token = window.localStorage.getItem('token');
+
+    if (!token) {
+        throw new Error("No token provided")
+    }
+
     const headers = {
         'X-CSRF-Token': csrfToken,
         Authorization: `Bearer ${token}`
@@ -11,16 +17,20 @@ const useFetch = () => {
 
     const request = async (url, method, body) => {
 
-        try {
+       try {
             const response = await fetch(url, {method, body, headers});
 
             if (!response.ok) {
                 throw new Error(`Could not fetch ${url}, status: ${response.status}`);
             }
-
-            return await response.json();
+            const data = await response.json();
+            if (data.error || data.errors) {
+                throw new Error(`Error in request ${url}: ${data.error} ${data.errors}`)
+            }
+            return data;
         } catch(e) {
-            throw e;
+           throw e
+            //console.log(e.message());
         }
     };
 
